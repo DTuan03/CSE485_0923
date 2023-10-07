29cf84093b88734a3f0db2fd775866f3d0b347f6
DTuan03 <tuanhttt1@gmail.com>
2023-10-07 14:25:22 +0700

    my project

--- BTTH03_CSE485/QuanLyPhongMach/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('APP_ROOT', dirname(__FILE__,2));
-
-// echo APP_ROOT;
-
-define('DOMAIN','http://localhost/BTTH03_CSE485/QuanLyPhongMach');
-
-// echo DOMAIN;
--- BTTH03_CSE485/QuanLyPhongMach/inc/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<!-- Latest compiled JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-</body>
-
-</html>
